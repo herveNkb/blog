2a5b605efc6fa00c5468f00ae36fa50aca0e486d
AirV <herve.jacotet@sfr.fr>
2023-04-08 20:26:42 +0200

    feat(keywords entity): create entity Created "keywords" entity, create SQL request with make:migration and push to DB with doctrine:migration:migrate.

---
+++ src/Entity/Keywords.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\KeywordsRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: KeywordsRepository::class)]
+class Keywords
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 50)]
+    private ?string $keyword = null;
+
+    #[ORM\Column(length: 60)]
+    private ?string $slug = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getKeyword(): ?string
+    {
+        return $this->keyword;
+    }
+
+    public function setKeyword(string $keyword): self
+    {
+        $this->keyword = $keyword;
+
+        return $this;
+    }
+
+    public function getSlug(): ?string
+    {
+        return $this->slug;
+    }
+
+    public function setSlug(string $slug): self
+    {
+        $this->slug = $slug;
+
+        return $this;
+    }
+}
