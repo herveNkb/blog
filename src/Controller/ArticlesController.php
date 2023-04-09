bfe2ee734a9c4fc933840b3c02a5891c1ccec769
AirV <herve.jacotet@sfr.fr>
2023-04-09 18:38:22 +0200

    feat(knp-paginator): articles pagination Adding method of knp-paginator for paginate data.

--- src/Controller/ArticlesController.php
+++ src/Controller/ArticlesController.php
@@ -4,7 +4,9 @@ namespace App\Controller;
 
 use App\Entity\Articles;
 use Doctrine\Persistence\ManagerRegistry;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -12,11 +14,21 @@ use Symfony\Component\Routing\Annotation\Route;
 class ArticlesController extends AbstractController
 {
     #[Route('/', name: 'app_articles')]
-    public function index(ManagerRegistry $doctrine): Response
+    public function index(ManagerRegistry $doctrine, Request $request, PaginatorInterface $paginator): Response
     {
-        // We call the list of all items in the database
-        // replaces getDoctrine() which is deprecated
-        $articles = $doctrine -> getRepository(Articles::class) -> findAll();
+        // findBy() method that retrieves data from the articles table with a descending date sort.
+        // Replaces getDoctrine() which is deprecated
+        $data = $doctrine -> getRepository(Articles::class) -> findBy([],
+        ['created_at' => 'DESC']);
+
+        // We paginate the data
+        $articles = $paginator -> paginate(
+            $data, // Data to paginate
+            $request -> query -> getInt('page', 1), // Current page number, 1 by default
+            1 // Number of items per page
+        );
+
+
 
         // We return the view with the list of articles
         return $this -> render('articles/index.html.twig', [
