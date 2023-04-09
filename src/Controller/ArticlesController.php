6c591bf23cbab49d0e4edc7ad1dd99227f0d5b48
AirV <herve.jacotet@sfr.fr>
2023-04-09 21:24:01 +0200

    feat(slug): display slug Add method for display slug if exist in DB for articles.

--- src/Controller/ArticlesController.php
+++ src/Controller/ArticlesController.php
@@ -19,7 +19,7 @@ class ArticlesController extends AbstractController
         // findBy() method that retrieves data from the articles table with a descending date sort.
         // Replaces getDoctrine() which is deprecated
         $data = $doctrine -> getRepository(Articles::class) -> findBy([],
-        ['created_at' => 'DESC']);
+            ['created_at' => 'DESC']);
 
         // We paginate the data
         $articles = $paginator -> paginate(
@@ -27,12 +27,29 @@ class ArticlesController extends AbstractController
             $request -> query -> getInt('page', 1), // Current page number, 1 by default
             1 // Number of items per page
         );
-
-
-
         // We return the view with the list of articles
         return $this -> render('articles/index.html.twig', [
             'articles' => $articles
+        ]);
+    }
+
+//    Method that allows to display the slug of the article
+    #[Route('/{slug}', name: 'article')]
+    public function article($slug, ManagerRegistry $doctrine): Response
+    {
+        // We retrieve the article with the slug
+        $article = $doctrine -> getRepository(Articles::class) -> findOneBy([
+            'slug' => $slug
+        ]);
+        // We check if an article is found?
+        if (!$article) {
+            // If not, we return a 404 error
+            throw $this -> createNotFoundException('L\'article n\'a pas été trouvé');
+        }
+        // If an article is found, we return the view with the article
+
+        return $this -> render('articles/article.html.twig', [
+            'article' => $article
         ]);
     }
 }
