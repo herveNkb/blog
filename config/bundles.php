4696d5f9eacc6bf414c6ef616ad9f98c541cfb24
AirV <herve.jacotet@sfr.fr>
2023-04-13 17:26:10 +0200

    chore(vich uploader): image upload Install vich uploader for uploaded image.

--- config/bundles.php
+++ config/bundles.php
@@ -15,4 +15,6 @@ return [
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
+    EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
+    Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
 ];
