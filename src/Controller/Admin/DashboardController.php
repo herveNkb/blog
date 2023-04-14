da8534b35bb92bc5ad87a67b71a8201adaa2b153
AirV <herve.jacotet@sfr.fr>
2023-04-14 09:06:22 +0200

    feat(admin): create dashboard Created the base of admin dashboard.

---
+++ src/Controller/Admin/DashboardController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Articles;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
+use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DashboardController extends AbstractDashboardController
+{
+    #[IsGranted('ROLE_ADMIN')]
+    #[Route('/admin', name: 'admin')]
+    public function index(): Response
+    {
+        return parent::index();
+    }
+
+    public function configureDashboard(): Dashboard
+    {
+        return Dashboard::new()
+            ->setTitle('Blog');
+    }
+
+    public function configureMenuItems(): iterable
+    {
+        yield MenuItem::linkToDashboard('Tableau de bord', 'fa fa-dashboard');
+         yield MenuItem::linkToCrud('articles', 'fas fa-list', Articles::class);
+    }
+}
