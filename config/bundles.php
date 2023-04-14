e88cdbb9eca8a5fd99c8a158e024173947fcb101
AirV <herve.jacotet@sfr.fr>
2023-04-14 11:21:56 +0200

    chore(slug): stof doctrine extensions Add bundle for Slug.

--- config/bundles.php
+++ config/bundles.php
@@ -17,4 +17,5 @@ return [
     Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
     Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
+    Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle::class => ['all' => true],
 ];
