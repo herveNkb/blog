596e814063a256f50c66fdc3ececb68e66d86c27
AirV <herve.jacotet@sfr.fr>
2023-04-14 19:24:03 +0200

    fix(login form): flash message Translate in french flash message when there is already an account with this email in french.

--- src/Entity/Users.php
+++ src/Entity/Users.php
@@ -11,7 +11,7 @@ use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 
 #[ORM\Entity(repositoryClass: UsersRepository::class)]
-#[UniqueEntity(fields: ['email'], message: 'There is already an account with this email')]
+#[UniqueEntity(fields: ['email'], message: 'Il existe déjà un compte avec cet email')]
 class Users implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
