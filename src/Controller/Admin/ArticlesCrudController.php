9f84eb1954f1dff439be8f8b3ac698fca8161f1a
AirV <herve.jacotet@sfr.fr>
2023-04-15 18:05:59 +0200

    feat(back office admin): field layout Layout of fields that must be visible or not, and display of articles in descending order.

--- src/Controller/Admin/ArticlesCrudController.php
+++ src/Controller/Admin/ArticlesCrudController.php
@@ -3,7 +3,11 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Articles;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextareaField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 
 class ArticlesCrudController extends AbstractCrudController
 {
@@ -12,14 +16,24 @@ class ArticlesCrudController extends AbstractCrudController
         return Articles::class;
     }
 
-    /*
+
+    // Configure the fields to be displayed
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
             TextField::new('title'),
-            TextEditorField::new('description'),
+            TextField::new('slug')->hideOnForm(), // hideOnForm() is used to hide the field on the new and edit pages
+            TextareaField::new('content')->hideOnIndex(), // hideOnIndex() is used to hide the field on the index page
+            DateField::new('createdAt')->hideOnForm(), // hideOnForm() is used to hide the field on the new and edit pages
+            TextField::new('featuredImage'),
         ];
     }
-    */
+
+    // Modify the ArticlesCrudController globally
+    public function configureCrud(Crud $crud): Crud
+    {
+        return $crud
+            // Puts items by creation date ascending
+            ->setDefaultSort(['created_at' => 'DESC']);
+    }
 }
