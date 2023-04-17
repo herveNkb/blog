e859a5f9fe5d593344a5c7a98cea248c7570a15d
AirV <herve.jacotet@sfr.fr>
2023-04-17 06:24:25 +0200

    feat(dashboard): field vich uploader add and configure field for upload image with vich uploader.

--- src/Controller/Admin/ArticlesCrudController.php
+++ src/Controller/Admin/ArticlesCrudController.php
@@ -6,8 +6,10 @@ use App\Entity\Articles;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\DateField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\TextareaField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ImageField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use Vich\UploaderBundle\Form\Type\VichImageType;
 
 class ArticlesCrudController extends AbstractCrudController
 {
@@ -21,11 +23,12 @@ class ArticlesCrudController extends AbstractCrudController
     public function configureFields(string $pageName): iterable
     {
         return [
-            TextField::new('title'),
-            TextField::new('slug')->hideOnForm(), // hideOnForm() is used to hide the field on the new and edit pages
-            TextareaField::new('content')->hideOnIndex(), // hideOnIndex() is used to hide the field on the index page
-            DateField::new('createdAt')->hideOnForm(), // hideOnForm() is used to hide the field on the new and edit pages
-            TextField::new('featuredImage'),
+            yield TextField::new('title', 'Titre'),
+            yield TextField::new('slug', 'Slug')->hideOnForm(), // hideOnForm() is used to hide the field on the new and edit pages
+            yield TextEditorField::new('content', 'Article')->hideOnIndex()->setNumOfRows(20), // hideOnIndex() is used to hide the field on the index page
+            yield DateField::new('createdAt', 'Date de création')->hideOnForm(), // hideOnForm() is used to hide the field on the new and edit pages
+            yield TextField::new('imageFile', 'Télécharger image')->setFormType(VichImageType::class)->onlyOnForms(), // onlyOnForms() is used to display the field only on the new and edit pages
+            yield ImageField::new('featured_image', 'Apercu')->setBasePath('uploads/images')->onlyOnIndex(), // onlyOnIndex() is used to display the field only on the index page
         ];
     }
 
