3c97c405e1df9260b32a6666138a42b957037cc8
AirV <herve.jacotet@sfr.fr>
2023-04-19 21:00:08 +0200

    feat(admin back-office): display fields Add image_title field for images tab.

--- src/Controller/Admin/ImagesCrudController.php
+++ src/Controller/Admin/ImagesCrudController.php
@@ -19,10 +19,10 @@ class ImagesCrudController extends AbstractCrudController
     public function configureFields(string $pageName): iterable
     {
         return [
-            yield DateField::new('updatedAt', 'Date de création')->hideOnForm(), // hideOnForm() is used to hide the field on the new and edit pages
-            yield TextField::new('imageFile', 'Taille maximum image : 2MB')->setFormType(VichImageType::class)->onlyOnForms(), // onlyOnForms() is used to display the field only on the new and edit pages
-            yield ImageField::new('image_name', 'Apercu')->setBasePath('uploads/front-images')->onlyOnIndex() // onlyOnIndex() is used to display the field only on the index page
+            yield TextField ::new('image_title', 'Titre de l\'image'),
+            yield DateField ::new('updatedAt', 'Date de création') -> hideOnForm(), // hideOnForm() is used to hide the field on the new and edit pages
+            yield TextField ::new('imageFile', 'Taille maximum image : 2MB') -> setFormType(VichImageType::class) -> onlyOnForms(), // onlyOnForms() is used to display the field only on the new and edit pages
+            yield ImageField ::new('image_name', 'Apercu') -> setBasePath('uploads/front-images') -> onlyOnIndex() // onlyOnIndex() is used to display the field only on the index page
         ];
     }
-
 }
