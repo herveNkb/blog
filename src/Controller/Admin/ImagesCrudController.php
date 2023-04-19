153e4f1a0d48e085bf470138d6a9bc49f3bf1c96
AirV <herve.jacotet@sfr.fr>
2023-04-19 19:30:58 +0200

    feat(admin back-office): display fields Add field for images tab.

--- src/Controller/Admin/ImagesCrudController.php
+++ src/Controller/Admin/ImagesCrudController.php
@@ -4,6 +4,10 @@ namespace App\Controller\Admin;
 
 use App\Entity\Images;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ImageField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use Vich\UploaderBundle\Form\Type\VichImageType;
 
 class ImagesCrudController extends AbstractCrudController
 {
@@ -12,14 +16,13 @@ class ImagesCrudController extends AbstractCrudController
         return Images::class;
     }
 
-    /*
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            yield DateField::new('updatedAt', 'Date de création')->hideOnForm(), // hideOnForm() is used to hide the field on the new and edit pages
+            yield TextField::new('imageFile', 'Taille maximum image : 2MB')->setFormType(VichImageType::class)->onlyOnForms(), // onlyOnForms() is used to display the field only on the new and edit pages
+            yield ImageField::new('image_name', 'Apercu')->setBasePath('uploads/front-images')->onlyOnIndex() // onlyOnIndex() is used to display the field only on the index page
         ];
     }
-    */
+
 }
