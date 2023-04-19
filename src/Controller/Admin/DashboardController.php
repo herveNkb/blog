a3a9fa79d0ee1aaa795292fd2564a2a7daa5d596
AirV <herve.jacotet@sfr.fr>
2023-04-19 18:13:45 +0200

    feat(admin back-office): menu Add tab into menu for images.

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -4,6 +4,7 @@ namespace App\Controller\Admin;
 
 use App\Entity\Categories;
 use App\Entity\Articles;
+use App\Entity\Images;
 use App\Entity\Users;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
@@ -36,6 +37,7 @@ class DashboardController extends AbstractDashboardController
             MenuItem ::linkToCrud('Édition', 'fas fa-book', Articles::class),
             MenuItem ::linkToCrud('Catégories', 'fas fa-list', Categories::class),
         ]);
+        yield MenuItem ::linkToCrud('Images', 'fas fa-image', Images::class);
         yield MenuItem ::linkToCrud('Profil', 'fas fa-users', Users::class);
         yield MenuItem ::linkToUrl('Retour à l\'accueil', 'fas fa-home', $this -> generateUrl('app_main'));
 
